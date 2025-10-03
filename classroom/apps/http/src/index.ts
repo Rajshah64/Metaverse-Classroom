ebe65b478a5cffbbf4fe60a66c3030f8ed0b2866
Raj shah <rajviralshah@gmail.com>
2025-10-03 13:14:03 +0530

    Created a struture of all the API endpoints

--- classroom/apps/http/src/index.ts
+++ classroom/apps/http/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { router } from "./routes/v1/index.js";
+const cors = require('cors');
 
 const app = express();
 
 app.use("/api/v1",router);
+app.use(cors())
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
--- classroom/apps/http/src/routes/v1/index.ts
+++ classroom/apps/http/src/routes/v1/index.ts
@@ -1,16 +1,33 @@
 import { Router } from "express";
+import { userRouter } from "./user.js";
+import { spaceRouter } from "./space.js";
+import { adminRouter } from "./admin.js";
 
 export const router = Router();
 
-router.get("/signup", (req, res) => {
+router.post("/signup",(req, res) => {
   res.json({
     message: "Signup"
   })
 });
 
 
-router.get("/signin", (req, res) => {
+router.post("/signin", (req, res) => {
   res.json({
     message: "Signin"
   });
 });
+
+//get all the elements for the arena 
+router.get("/elements",(req,res )=>{
+
+})
+
+//get all available avatars in the profile section 
+router.get("/avatars",(req,res )=>{
+
+})
+
+router.use("/user", userRouter);
+router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
